61ad80bd84e5c710ee7f7ef9361ffe0169968c88
danggro <anangdwin55@gmail.com>
2024-02-19 09:27:06 +0700

    add: authentication apps, template, urls

--- src/django_web/settings.py
+++ src/django_web/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "landing.apps.LandingConfig",
+    "authentication.apps.AuthenticationConfig",
 ]
 
 MIDDLEWARE = [
--- src/django_web/urls.py
+++ src/django_web/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("landing.urls")),
+    path("authentication/", include("authentication.urls")),
 ]

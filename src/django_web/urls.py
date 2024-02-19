9b98d6b71a9a08445a9587e1f34cf54a91df6a48
danggro <anangdwin55@gmail.com>
2024-02-19 09:29:02 +0700

    add: blog apps, template, urls

--- src/django_web/urls.py
+++ src/django_web/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("landing.urls")),
     path("authentication/", include("authentication.urls")),
+    path("blog/", include("blog.urls")),
 ]

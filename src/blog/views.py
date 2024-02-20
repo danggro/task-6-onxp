a4505c031bc1b3e9e6197847d560c509f8cf394f
danggro <anangdwin55@gmail.com>
2024-02-20 11:45:33 +0700

    add: blog list

--- src/blog/views.py
+++ src/blog/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def base(request):
-    return render(request, "blog/index.html")
+    context = {
+        "blogs" : ["Cara Mengubah Code Menjadi Uang", "Python, Anaconda, Cobra Merupakan Nama Jenis Ular"]
+    }
+    return render(request, "blog/index.html", context)
